d34397fcf2533d299104cfcd08c9963a5f7373a9
Slavi <slavi@users-MacBook-Pro.local>
2022-03-29 18:23:50 +0300

    heroku changes #2

--- pintereso/settings.py
+++ pintereso/settings.py
@@ -131,6 +131,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT  = BASE_DIR / 'staticfiles',
+
 STATICFILES_DIRS = (
     BASE_DIR / 'staticfiles',
 )
