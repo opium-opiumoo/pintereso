4a448de16fc685260d0393780b1da94670b1a220
Slavi <slavi@users-MacBook-Pro.local>
2022-03-29 18:41:57 +0300

    fixed DB settings

--- pintereso/settings.py
+++ pintereso/settings.py
@@ -87,10 +87,10 @@ WSGI_APPLICATION = 'pintereso.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'pintereso_db',
-        'USER': 'postgres',
-        'PASSWORD': '1123QwER',
-        'HOST': '127.0.0.1',
+        'NAME': 'd720dkuachf9hq',
+        'USER': 'ojmykspkkzniyr',
+        'PASSWORD': '0068afd9603e5f18ceea6296f347aaff99d78152ada066c340faaf647df6d800',
+        'HOST': 'ec2-99-80-170-190.eu-west-1.compute.amazonaws.com',
         'PORT': '5432',
     }
 }
