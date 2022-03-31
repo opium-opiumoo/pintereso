878f17d472c2a11e43498d9a7cc8230f7d438fb8
opium-opiumoo <slavi6pm@gmail.com>
2022-03-31 18:54:54 +0300

    test

--- pintereso/settings.py
+++ pintereso/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
 from pathlib import Path
-from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 
 # UPDATE secret key
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-0ldi_*e1yy15&lr=41vj^=janw)gp+$y%7ztx!5lq=52*ickuy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
