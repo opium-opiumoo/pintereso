16a5c4afa240f6c0106aa34e6ad4ef3f87a11674
opium-opiumoo <slavi6pm@gmail.com>
2022-04-10 15:20:51 +0300

    deployment fixes #3

--- pintereso/urls.py
+++ pintereso/urls.py
@@ -15,11 +15,16 @@ Including another URLconf
 """
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pintereso.web.urls')),
     path('account/', include('pintereso.accounts.urls')),
-] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+    re_path(r'^', include('pintereso.urls')),
+
+    re_path(r'^static/(?P<path>.*)$', serve,
+            {'document_root': settings.STATIC_ROOT}),
+]
