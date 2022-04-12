e29fb3427bc7a8734c71a9792b2270fb144e3a12
opium-opiumoo <slavi6pm@gmail.com>
2022-04-12 21:34:20 +0300

    added logging

--- pintereso/settings.py
+++ pintereso/settings.py
@@ -164,6 +164,45 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TAGGIT_CASE_INSENSITIVE = True
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': ('%(asctime)s [%(process)d] [%(levelname)s] '
+                       'pathname=%(pathname)s lineno=%(lineno)s '
+                       'funcname=%(funcName)s %(message)s'),
+            'datefmt': '%Y-%m-%d %H:%M:%S'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        }
+    },
+    'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'logging.NullHandler',
+        },
+        'console': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+        'django.request': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+    }
+}
+
 cloudinary.config(
   cloud_name = config('CLOUDINARY_CLOUD_NAME', None),
   api_key = config('CLOUDINARY_API_KEY', None),
